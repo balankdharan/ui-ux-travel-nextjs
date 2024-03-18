fbe0eacde9902dbfc52ed877d9931c14f1be6957
balankdharan <balankdharan@gmail.com>
2024-03-18 20:32:44 +0530

    some section added

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
 import React from "react";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image
+            src="/phone.png"
+            alt="phone"
+            width={440}
+            height={1000}
+            className="feature-phone"
+          />
+        </div>
+        <div className="z-20 flex w-full flex-col lg:w-[60%]">
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,7 +1,62 @@
+import Image from "next/image";
 import React from "react";
 
 const Guide = () => {
-  return <div>Guide</div>;
+  return (
+    <section className="flexCenter flex-col">
+      <div className="padding-container max-container w-full pb-24">
+        <Image src={"/camp.svg"} alt="camp" height={50} width={50} />
+        <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+          WE ARE HERE FOR YOU
+        </p>
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
+            Guide you to Easy Path
+          </h2>
+          <p className="regular-16 text-gray-30 xl:max-w-[520px]">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Praesentium soluta doloribus numquam id tenetur corporis repudiandae
+            laboriosam voluptate. Necessitatibus illo provident asperiores dolor
+            molestias soluta. Repudiandae rerum non placeat recusandae!
+          </p>
+        </div>
+      </div>
+
+      <div className="flexCenter max-container relative w-full">
+        <Image
+          src={"/boat.png"}
+          alt="boat"
+          height={580}
+          width={1440}
+          className="w-full object-cover object-center 2xl:rounded-5xl"
+        />
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <Image
+            src={"/meter.svg"}
+            alt="meter"
+            height={16}
+            width={158}
+            className="h-full w-auto"
+          />
+          <div className="flex-between flex-col ">
+            <div className="flex w-full flex-col">
+              <div className="flex-between w-full">
+                <p className="regular-16 text-gray-20">Destination</p>
+                <p className="bold-16 text-green-50">48 min</p>
+              </div>
+              <p className="bold-20 mt-2">Aguas Calientes</p>
+            </div>
+            <div className="flex w-full flex-col">
+              <p className="regular-16 text-gray-20">Start track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">
+                Wonorejo Pasuruan
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Guide;
